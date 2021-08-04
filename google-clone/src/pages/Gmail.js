5a1606af64f0abb3ecf425301821b3943ba6fa63
MorganBare <morganjbare@gmail.com>
2021-08-04 13:23:13 +0200

    finished project

---
+++ google-clone/src/pages/Gmail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from '../styles/center';
+
+const Gmail = () => {
+    return (
+        <Container>
+            <h1>GMAIL</h1>
+        </Container>
+    )
+}
+
+export default Gmail
